fbe45414ce930993e986fd0ff40c4e4826b0d296
Hubert Sitarski <hubert.sitarski@bloomboard.co>
2020-05-10 16:10:33 +0200

    Entities and Repos

---
+++ src/Entity/ProductOrder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductOrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductOrderRepository::class)
+ */
+class ProductOrder extends BaseEntity
+{
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class, inversedBy="productOrders")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $product;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Order::class, inversedBy="productOrders")
+     * @ORM\JoinColumn(nullable=false, name="order_id")
+     */
+    private $orderEntity;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="decimal", precision=5, scale=2)
+     */
+    private $price;
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getOrderEntity(): ?Order
+    {
+        return $this->orderEntity;
+    }
+
+    public function setOrderEntity(?Order $orderEntity): self
+    {
+        $this->orderEntity = $orderEntity;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
